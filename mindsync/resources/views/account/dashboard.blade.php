e942e901a1d0f1ba28a2d56a88671418ab9ebc43
Jakub Selonke <kubase477@gmail.com>
2025-05-03 13:45:09 +0200

    Pełny responsive dashboard
    Do poprawy tooltipy w mobile aside

--- mindsync/resources/views/account/dashboard.blade.php
+++ mindsync/resources/views/account/dashboard.blade.php
@@ -9,9 +9,9 @@
     <script src="https://kit.fontawesome.com/086b12d3c8.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="overflow-hidden flex min-h-screen">
+<body class="overflow-x-hidden flex min-h-screen">
     <!-- Sidebar -->
-    <aside class="fixed w-[350px] h-screen overflow-y-auto bg-bg-secondary py-10 px-7">
+    <aside class="fixed w-[300px] 2xl:w-[350px] h-screen overflow-y-auto bg-bg-secondary py-10 px-7 hidden 2lg:block">
         <a href="/dashboard" class="block text-4xl text-center font-semibold mb-10">
             Mind<span class="text-accent">Sync</span>
         </a>
@@ -47,60 +47,137 @@
             </button>
         </form>
     </aside>
+    
+    <!-- Mobile Sidebar -->
+    <div class="fixed left-0 top-0 bottom-0 z-50 2lg:hidden">
+        <aside id="asideMobile" class="w-[60px] xsm:w-[80px] h-screen overflow-y-auto bg-bg-secondary xsm:px-2 py-10 text-center">
+            <a href="/dashboard" class="block font-semibold mb-10 leading-none text-xl">
+                Mind
+                <span class="text-accent">Sync</span>
+            </a>
+            <div class="space-y-2 w-[50px] m-auto">
+                <div class="relative group">
+                    <a href="/settings" class="block bg-bg-tint rounded-xl p-2 cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
+                        <i class="fa-solid fa-user text-accent"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Ustawienia</div>
+                </div>
+                <div class="relative group">
+                    <a href="/dashboard" class="block rounded-xl p-2 bg-accent text-white border-bg-secondary">
+                        <i class="fa-solid fa-chart-simple"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Dashboard</div>
+                </div>
+                <div class="relative group">
+                    <a href="/emotion_journal" class="block rounded-xl p-2 cursor-pointer text-gray-500 hover:ring-2 hover:ring-accent transition duration-300">
+                        <i class="fa-solid fa-book-open"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Dziennik nastroju</div>
+                </div>
+                <div class="relative group">
+                    <a href="/mind_exercises" class="block rounded-xl p-2 cursor-pointer text-gray-500 hover:ring-2 hover:ring-accent transition duration-300">
+                        <i class="fa-solid fa-spa"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Ćwiczenia mindfulness</div>
+                </div>
+                <div class="relative group">
+                    <a href="/chatbot" class="block rounded-xl p-2 cursor-pointer text-gray-500 hover:ring-2 hover:ring-accent transition duration-300">
+                        <i class="fa-solid fa-message"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Chatbot wsparcia</div>
+                </div>
+                <div class="relative group">
+                    <a href="/emotions_raport" class="block rounded-xl p-2 cursor-pointer text-gray-500 hover:ring-2 hover:ring-accent transition duration-300">
+                        <i class="fa-solid fa-face-laugh"></i>
+                    </a>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Analiza emocji</div>
+                </div>
+            </div>
+            <form action="{{ route('logout') }}" method="POST" class="absolute bottom-10 left-1/2 -translate-x-1/2">
+                @csrf
+                <div class="relative group">
+                    <button type="submit" class="w-[50px] cursor-pointer text-gray-500 hover:text-red-400">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </button>
+                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Wyloguj</div>
+                </div>
+            </form>
+        </aside>
+    </div>
 
     <!-- Main Content -->
-    <main class="flex-1 ml-[350px] h-screen overflow-y-auto p-10 space-y-6">
-        <div class="bg-bg-tint p-6 px-15 rounded-xl relative overflow-hidden">
-            <h2 class="text-3xl font-semibold text-header-gray">Miłego Poranka, Jakub 👋</h2>
-            <p class="text-gray-500 mt-2">Powodzenia w kontynuacji twojej 14 dniowej passy medytacji</p>
-            <img src="/images/leaf1.svg" class="absolute left-[-111px] top-[-104px] z-3" alt="leaf1">   
+    <main class="flex-1 ml-[60px] xsm:ml-[80px] 2lg:ml-[300px] 2xl:ml-[350px] h-screen overflow-y-auto  p-2 md:p-10 space-y-6">
+        <div class="bg-bg-tint p-6 md:px-15 rounded-xl relative overflow-hidden">
+            <h2 class="text-xl md:text-2xl 2xl:text-3xl font-semibold text-header-gray">Miłego Poranka, Jakub 👋</h2>
+            <p class="text-gray-500 mt-2 text-sm md:text-base">Powodzenia w kontynuacji twojej 14 dniowej passy medytacji</p>
+            <img src="/images/leaf1.svg" class="absolute left-[-100%] md:left-[-111px] top-[-104px] z-3" alt="leaf1">   
             <img src="/images/leaf2.svg" class="absolute left-[596px] top-[-75px]" alt="leaf2 z-1">   
             <img src="/images/leaf3.svg" class="absolute right-[-200%] 2xl:right-0! bottom-[-110px] z-0" alt="leaf3">   
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-6 min-h-60">
+        <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-6 min-h-60">
             <div class="bg-bg-tint p-6 rounded-xl text-center ">
-                <p class="text-2xl text-header-gray font-semibold">Twój średni nastrój</p>
-                <div class="text-6xl font-bold text-accent mt-10">70%</div>
+                <p class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold">Twój średni nastrój</p>
+                <div class="text-4xl md:text-5xl 2xl:text-6xl font-bold text-accent mt-4 2xl:mt-10">70%</div>
             </div>
             <div class="bg-bg-tint p-6 rounded-xl text-center">
-                <p class="text-2xl text-header-gray font-semibold">Streak aktywnych dni</p>
-                <div class="text-6xl font-bold text-accent mt-10">14</div>
+                <p class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold">Streak aktywnych dni</p>
+                <div class="text-4xl md:text-5xl 2xl:text-6xl font-bold text-accent mt-4 2xl:mt-10">14</div>
             </div>
-            <div class="bg-bg-tint p-6 rounded-xl text-center relative">
-                <p class="text-2xl text-header-gray font-semibold">Twoje wpisy</p>
-                <div class="text-6xl font-bold text-accent mt-10">253</div>
-                <img src="/images/chart-graphic.svg" class="h-[50px] absolute bottom-4 left-1/2 -translate-x-1/2" alt="Wykres nastroju">
+            <div class="bg-bg-tint p-6 rounded-xl text-center relative overflow-y-hidden">
+                <p class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold">Twoje wpisy</p>
+                <div class="text-4xl md:text-5xl 2xl:text-6xl font-bold text-accent mt-4 2xl:mt-10">253</div>
+                <img src="/images/chart-graphic.svg" class="bottom-[-100%] h-[50px] absolute 2xl:bottom-4 left-1/2 -translate-x-1/2" alt="Wykres nastroju">
             </div>
             <div class="bg-bg-tint p-6 rounded-xl text-center">
-                <p class="text-2xl text-header-gray font-semibold">Porada na dziś</p>
-                <p class="text-gray-500 mt-2">Porada odświeżająca się raz dziennie lub przy każdym odwiedzeniu dashboard (do zdecydowania), można znaleść jakieś API. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptate? </p>
+                <p class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold">Porada na dziś</p>
+                <p class="text-xs md:text-sm 2xl:text-base text-gray-500 mt-2">Porada odświeżająca się raz dziennie lub przy każdym odwiedzeniu dashboard (do zdecydowania), można znaleść jakieś API. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptate? </p>
             </div>
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-6 h-100">
-            <div class="bg-bg-tint p-6 rounded-xl col-span-3">
-                <p class="text-2xl text-header-gray font-semibold text-center mb-5">Wykres nastroju z ostatnich 30 dni</p>
-                <div class="bg-gradient-to-b from-teal-300 to-white rounded h-full max-h-70">
+        <div class="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-6 min-h-100">
+            <div class="bg-bg-tint p-6 rounded-xl lg:col-span-2 2xl:col-span-3">
+                <p class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold text-center mb-5">Wykres nastroju z ostatnich 30 dni</p>
+                <div class="bg-gradient-to-b from-teal-300 to-white rounded">
                     <canvas id="moodChart"></canvas>
                 </div>
             </div>
             <div class="grid grid-rows-2 h-full gap-6">
-                <a href="emotions_journal" class="block bg-bg-tint p-4 rounded-xl relative cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
-                    <h3 class="text-2xl text-header-gray font-semibold mb-2">Twój ostatni wpis</h3>
-                    <p class="text-sm text-gray-500">Lorem Ipsum dolore set amet ipsum randon, lorus deus machina...</p>
-                    <p class="text-sm text-gray-400 absolute bottom-4">09.04.2025</p>
-                    <i class="fa-solid fa-chevron-right text-accent text-3xl absolute bottom-4 right-4"></i>
+                <a href="emotions_journal" class="block bg-bg-tint pr-8 md:pr-4 p-4 rounded-xl relative cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
+                    <h3 class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold mb-2">Twój ostatni wpis</h3>
+                    <p class="text-xs md:text-sm text-gray-500">Lorem Ipsum dolore set amet ipsum randon, lorus deus machina...</p>
+                    <p class="text-xs md:text-sm text-gray-400 absolute bottom-4">09.04.2025</p>
+                    <i class="fa-solid fa-chevron-right text-accent text-2xl md:text-3xl absolute bottom-4 right-4"></i>
                 </a>
-                <a href="/mind_exercises" class="block bg-bg-tint p-4 rounded-xl relative cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
-                    <h3 class="text-2xl text-header-gray font-semibold mb-2">Polecane ćwiczenie</h3>
-                    <p class="text-sm text-accent-strong ">2h Spacer - Łatwy</p>
-                    <p class="text-sm text-gray-400 mt-2">Opis ćwiczenia Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                    <i class="fa-solid fa-chevron-right text-accent text-3xl absolute bottom-4 right-4"></i>
+                <a href="/mind_exercises" class="block bg-bg-tint pr-8 md:pr-4 p-4 rounded-xl relative cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
+                    <h3 class="text-lg md:text-xl 2xl:text-2xl text-header-gray font-semibold mb-2">Polecane ćwiczenie</h3>
+                    <p class="text-xs md:text-sm text-accent-strong ">2h Spacer - Łatwy</p>
+                    <p class="text-xs md:text-sm text-gray-400 mt-2">Opis ćwiczenia Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    <i class="fa-solid fa-chevron-right text-accent text-2xl md:text-3xl absolute bottom-4 right-4"></i>
                 </a>
             </div>
         </div>
     </main>
+     <script>
+        // Upewnij się, że tooltips są zawsze widoczne w viewport
+        document.addEventListener("DOMContentLoaded", function() {
+            const groups = document.querySelectorAll(".group");
+            
+            groups.forEach(group => {
+                group.addEventListener("mouseenter", function() {
+                    const tooltip = this.querySelector("div[class*='fixed']");
+                    if (tooltip) {
+                        // Pobierz pozycję przycisku
+                        const button = this.querySelector("a, button");
+                        const buttonRect = button.getBoundingClientRect();
+                        
+                        // Ustaw pozycję tooltipa względem przycisku
+                        tooltip.style.top = (buttonRect.top + buttonRect.height/2 - tooltip.offsetHeight/2) + "px";
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 
 </html>
