076ca1d6915d82e74ad079f939c027af4a5907ab
Jakub Selonke <kubase477@gmail.com>
2025-05-15 12:36:24 +0200

    Dodano ustawienia użytkownika #21

--- mindsync/resources/views/account/settings.blade.php
+++ mindsync/resources/views/account/settings.blade.php
@@ -9,10 +9,90 @@
     <script src="https://kit.fontawesome.com/086b12d3c8.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="overflow-x-hidden flex min-h-screen">
+<body class="flex min-h-screen overflow-x-hidden">
     @include('partials.aside')
-    <main>
-        <h1>Ustawienia użytkownika</h1>
+    <main class="flex-1 space-y-6 p-2 md:p-10 h-screen overflow-y-auto">
+        <h1 class="text-header-gray text-2xl">Ustawienia</h1>
+        <div class="gap-6 grid grid-cols-1 lg:grid-cols-2">
+ 
+            <div class="bg-bg-tint p-6 rounded-md">
+                <h2 class="mb-4 pb-2 border-gray-400 border-b-2 font-semibold text-lg">Moje Konto</h2>
+                <div class="flex items-center gap-4 mb-4">
+                    <div class="bg-gray-300 rounded-full w-16 h-16"></div>
+                    <button class="hover:bg-accent px-8 py-1 border border-teal-500 rounded-xl text-teal-500 hover:text-white text-center transition duration-300 cursor-pointer">Zmień</button>
+                </div>
+                <div class="space-y-3">
+                    <div>
+                        <label class="text-gray-600 text-sm">Nazwa</label>
+                        <input type="text" value="Jakub Selonke" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500">
+                    </div>
+                    <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
+                        <div>
+                            <label class="text-gray-600 text-sm">Email</label>
+                            <input type="email" value="49348@student.umg.edu.pl" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500" >
+                        </div>
+                        <div>
+                            <label class="text-gray-600 text-sm">Nr telefonu</label>
+                            <input type="text" value="111 222 333" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500" >
+                        </div>
+                        <div>
+                            <label class="text-gray-600 text-sm">Data urodzenia</label>
+                            <input type="text" value="35.02.1802" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500" >
+                        </div>
+                        <div>
+                            <label class="text-gray-600 text-sm">Płeć</label>
+                            <input type="text" value="Mężczyzna" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500" >
+                        </div>
+                    </div>
+                    <div>
+                        <label class="text-gray-600 text-sm">Bio</label>
+                        <textarea rows="8" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500 resize-none" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo neque,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo neque,</textarea>
+                    </div>
+                    <div class="flex justify-end">
+                        <button class="bg-accent mt-2 px-6 py-2 rounded-xl text-white transition duration-300 hover:bg-accent-strong cursor-pointer">Zapisz</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-col gap-6">
+                <div class="bg-bg-tint p-6 rounded-md">
+                    <h2 class="mb-4 pb-2 border-b font-semibold text-lg">Zmień hasło</h2>
+                    <div class="space-y-3">
+                        <input type="password" placeholder="Stare hasło" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500">
+                        <input type="password" placeholder="Nowe hasło" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500">
+                        <input type="password" placeholder="Nowe hasło ponownie" class="bg-bg-main px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-accent w-full text-gray-500">
+                        <div class="flex justify-end">
+                            <button class="bg-accent px-6 py-2 rounded-xl text-white transition duration-300 hover:bg-accent-strong cursor-pointer">Zapisz</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-bg-tint p-6 rounded-md">
+                    <h2 class="mb-4 pb-2 border-b font-semibold text-lg">Ustawienia prywatności</h2>
+                    <div class="space-y-3">
+                        <div class="flex items-center space-x-2">
+                            <input type="checkbox" id="data" class="w-4 h-4 accent-accent">
+                            <label for="data" class="text-gray-700 text-sm">Zgoda na przetwarzanie danych</label>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <input type="checkbox" id="notifications" class="w-4 h-4 accent-accent">
+                            <label for="notifications" class="text-gray-700 text-sm">Powiadomienia</label>
+                        </div>
+                        <div class="flex justify-end">
+                            <button class="bg-accent px-6 py-2 rounded-xl text-white transition duration-300 hover:bg-accent-strong cursor-pointer">Zapisz</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-bg-tint p-6 rounded-md">
+                    <h2 class="mb-2 font-semibold text-red-600 text-lg">Usuwanie konta</h2>
+                    <p class="mb-4 text-black text-sm">Tej akcji nie można cofnąć! Wszystkie twoje dane zostaną usunięte.</p>
+                    <div class="flex justify-end">
+                        <button class="bg-red-600 hover:bg-red-700 px-6 py-2 rounded-xl text-white transition duration-300 cursor-pointer">Usuń konto</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </body>
 </html>
--- mindsync/resources/views/partials/aside.blade.php
+++ mindsync/resources/views/partials/aside.blade.php
@@ -1,109 +1,109 @@
 <div id="asideWrapper" class="mr-[60px] xsm:mr-[80px] 2lg:mr-[300px] 2xl:mr-[350px]">
     <!-- Sidebar -->
-    <aside class="fixed w-[300px] 2xl:w-[350px] h-screen overflow-y-auto bg-bg-secondary py-10 px-7 hidden 2lg:block">
-        <a href="/" class="block text-4xl text-center font-semibold mb-10">
+    <aside class="hidden 2lg:block fixed bg-bg-secondary px-7 py-10 w-[300px] 2xl:w-[350px] h-screen overflow-y-auto">
+        <a href="/" class="block mb-10 font-semibold text-4xl text-center">
             Mind<span class="text-accent">Sync</span>
         </a>
-        <a href="/settings" class="mb-6 bg-bg-tint rounded-xl p-4 flex justify-between items-center cursor-pointer hover:ring-2 hover:ring-accent transition duration-300 relative">
+        <a href="/settings" class=" mb-6 bg-bg-tint rounded-xl p-4 flex justify-between items-center cursor-pointer hover:ring-2 hover:ring-accent transition duration-300 relative">
             <div class="ml-6">
-                <p class="text-sm font-semibold">{{$userName}}</p>
-                <p class="text-xs text-gray-500">{{$email}}</p>
+                <p class="font-semibold text-sm">{{$userName}}</p>
+                <p class="text-gray-500 text-xs">{{$email}}</p>
             </div>
-            <i class="fa-solid fa-chevron-right text-gray-500"></i>
-            <i class="fa-solid fa-user absolute text-accent"></i>
+            <i class="fa-chevron-right text-gray-500 fa-solid"></i>
+            <i class="absolute text-accent fa-solid fa-user"></i>
         </a>
         <nav class="space-y-4">
             <a href="/dashboard"
             class="asideLink {{ Request::is('dashboard') ? 'asideLink--active' : 'asideLink--normal' }}">
-                <i class="fa-solid fa-chart-simple mr-2"></i>Dashboard
+                <i class="mr-2 fa-solid fa-chart-simple"></i>Dashboard
             </a>
 
             <a href="/emotions/journal"
             class="asideLink {{ Request::is('emotions/journal*') ? 'asideLink--active' : 'asideLink--normal' }}">
-                <i class="fa-solid fa-book-open mr-2"></i>Dziennik nastroju
+                <i class="mr-2 fa-solid fa-book-open"></i>Dziennik nastroju
             </a>
 
             <a href="/mindExercises"
             class="asideLink {{ Request::is('mindExercises*') ? 'asideLink--active' : 'asideLink--normal' }}">
-                <i class="fa-solid fa-spa mr-2"></i>Ćwiczenia mindfulness
+                <i class="mr-2 fa-solid fa-spa"></i>Ćwiczenia mindfulness
             </a>
 
             <a href="/chatbot"
             class="asideLink {{ Request::is('chatbot') ? 'asideLink--active' : 'asideLink--normal' }}">
-                <i class="fa-solid fa-message mr-2"></i>Chatbot wsparcia
+                <i class="mr-2 fa-solid fa-message"></i>Chatbot wsparcia
             </a>
 
             <a href="/emotions/report"
             class="asideLink {{ Request::is('emotions/report') ? 'asideLink--active' : 'asideLink--normal' }}">
-                <i class="fa-solid fa-face-laugh mr-2"></i>Analiza emocji
+                <i class="mr-2 fa-solid fa-face-laugh"></i>Analiza emocji
             </a>
         </nav>
 
-        <form action="{{ route('logout') }}" method="POST" class="absolute bottom-10">
+        <form action="{{ route('logout') }}" method="POST" class="bottom-10 absolute">
             @csrf
-            <button type="submit" class="w-full text-left px-4 text-gray-500 cursor-pointer hover:text-red-400">
-                <i class="fas fa-sign-out-alt mr-2"></i> Wyloguj się
+            <button type="submit" class="px-4 w-full text-gray-500 hover:text-red-400 text-left cursor-pointer">
+                <i class="mr-2 fas fa-sign-out-alt"></i> Wyloguj się
             </button>
         </form>
     </aside>
     
     <!-- Mobile Sidebar -->
-    <div class="fixed left-0 top-0 bottom-0 z-50 2lg:hidden ">
-        <aside id="asideMobile" class="w-[60px] xsm:w-[80px] h-screen overflow-y-auto bg-bg-secondary xsm:px-2 py-10 text-center">
-            <a href="/dashboard" class="block font-semibold mb-10 leading-none text-xl">
+    <div class="2lg:hidden top-0 bottom-0 left-0 z-50 fixed">
+        <aside id="asideMobile" class="bg-bg-secondary xsm:px-2 py-10 w-[60px] xsm:w-[80px] h-screen overflow-y-auto text-center">
+            <a href="/dashboard" class="block mb-10 font-semibold text-xl leading-none">
                 Mind
                 <span class="text-accent">Sync</span>
             </a>
-            <div class="space-y-2 w-[50px] m-auto">
-                <div class="relative group">
-                    <a href="/settings" class="block bg-bg-tint rounded-xl p-2 cursor-pointer hover:ring-2 hover:ring-accent transition duration-300">
-                        <i class="fa-solid fa-user text-accent"></i>
+            <div class="space-y-2 m-auto w-[50px]">
+                <div class="group relative">
+                    <a href="/settings" class="block bg-bg-tint p-2 rounded-xl hover:ring-2 hover:ring-accent transition duration-300 cursor-pointer">
+                        <i class="text-accent fa-solid fa-user"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Ustawienia</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Ustawienia</div>
                 </div>
-                <div class="relative group">
+                <div class="group relative">
                     <a href="/dashboard" class="block rounded-xl p-2 cursor-pointer
                     {{ Request::is('dashboard') ? 'asideLink--active' : 'asideLink--normal text-gray-500' }}">
                         <i class="fa-solid fa-chart-simple"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Dashboard</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Dashboard</div>
                 </div>
-                <div class="relative group">
+                <div class="group relative">
                     <a href="/emotions/journal" class="block rounded-xl p-2 cursor-pointer
                     {{ Request::is('emotions/journal*') ? 'asideLink--active' : 'asideLink--normal text-gray-500' }}">
                         <i class="fa-solid fa-book-open"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Dziennik nastroju</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Dziennik nastroju</div>
                 </div>
-                <div class="relative group">
+                <div class="group relative">
                     <a href="/mindExercises" class="block rounded-xl p-2 cursor-pointer
                     {{ Request::is('mindExercises*') ? 'asideLink--active' : 'asideLink--normal text-gray-500' }}">
                         <i class="fa-solid fa-spa"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Ćwiczenia mindfulness</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Ćwiczenia mindfulness</div>
                 </div>
-                <div class="relative group">
+                <div class="group relative">
                     <a href="/chatbot" class="block rounded-xl p-2 cursor-pointer
                     {{ Request::is('chatbot') ? 'asideLink--active' : 'asideLink--normal text-gray-500' }}">
                         <i class="fa-solid fa-message"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Chatbot wsparcia</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Chatbot wsparcia</div>
                 </div>
-                <div class="relative group">
+                <div class="group relative">
                     <a href="/emotions/report" class="block rounded-xl p-2 cursor-pointer
                     {{ Request::is('emotions/report') ? 'asideLink--active' : 'asideLink--normal text-gray-500' }}">
                         <i class="fa-solid fa-face-laugh"></i>
                     </a>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Analiza emocji</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Analiza emocji</div>
                 </div>
             </div>
-            <form action="{{ route('logout') }}" method="POST" class="absolute bottom-10 left-1/2 -translate-x-1/2">
+            <form action="{{ route('logout') }}" method="POST" class="bottom-10 left-1/2 absolute -translate-x-1/2">
                 @csrf
-                <div class="relative group">
-                    <button type="submit" class="w-[50px] cursor-pointer text-gray-500 hover:text-red-400">
+                <div class="group relative">
+                    <button type="submit" class="w-[50px] text-gray-500 hover:text-red-400 cursor-pointer">
                         <i class="fas fa-sign-out-alt"></i>
                     </button>
-                    <div class="fixed z-50 left-[80px] top-auto whitespace-nowrap text-sm bg-gray-800 text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300 pointer-events-none">Wyloguj</div>
+                    <div class="top-auto left-[80px] z-50 fixed bg-gray-800 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-sm whitespace-nowrap transition duration-300 pointer-events-none">Wyloguj</div>
                 </div>
             </form>
         </aside>
