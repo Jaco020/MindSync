e07932d3a16fbd18b239adfc4179024732b1b339
Jakub Selonke <kubase477@gmail.com>
2025-05-16 17:09:18 +0200

    Interfejs Analia Emocji #16
    + Dodatkowe poprawki wizualne

--- mindsync/resources/views/account/dashboard.blade.php
+++ mindsync/resources/views/account/dashboard.blade.php
@@ -15,7 +15,7 @@
     <!-- Main Content -->
     <main class="flex-1 space-y-6 p-2 md:p-10 h-screen overflow-y-auto">
         <div class="relative bg-bg-tint p-6 md:px-15 rounded-xl overflow-hidden">
-            <h2 class="text-xl md:text-2xl 2xl:text-3xl">
+            <h2 class="text-xl md:text-2xl">
                 <span id="timeGreeting"></span>, {{ $userName }} 👋
             </h2>
             <!--<p class="mt-2 text-gray-500 text-sm md:text-base">Powodzenia w kontynuacji twojej 14 dniowej passy medytacji</p>-->
@@ -24,44 +24,44 @@
             <img src="/images/leaf3.svg" class="right-[-200%] 2xl:right-0! bottom-[-110px] z-0 absolute pointer-events-none" alt="leaf3">   
         </div>
 
-        <div class="gap-6 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 min-h-60">
+        <div class="gap-6 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 min-h-50">
             <div class="bg-bg-tint p-6 rounded-xl text-center">
-                <p class="font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl">Twój średni nastrój</p>
-                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl 2xl:text-6xl">{{$avgMood}}</div>
+                <p class="font-semibold text-header-gray text-lg md:text-xl">Twój średni nastrój</p>
+                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl">{{$avgMood}}</div>
             </div>
             <div class="bg-bg-tint p-6 rounded-xl text-center">
-                <p class="font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl">Streak aktywnych dni</p>
-                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl 2xl:text-6xl">-</div>
+                <p class="font-semibold text-header-gray text-lg md:text-xl">Streak aktywnych dni</p>
+                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl">-</div>
             </div>
             <div class="relative bg-bg-tint p-6 rounded-xl overflow-y-hidden text-center">
-                <p class="font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl">Twoje wpisy</p>
-                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl 2xl:text-6xl">{{$journalEntriesCount}}</div>
-                <img src="/images/chart-graphic.svg" class="bottom-[-100%] 2xl:bottom-4 left-1/2 absolute h-[50px] -translate-x-1/2" alt="Wykres nastroju">
+                <p class="font-semibold text-header-gray text-lg md:text-xl">Twoje wpisy</p>
+                <div class="mt-4 2xl:mt-10 font-bold text-accent text-4xl md:text-5xl">{{$journalEntriesCount}}</div>
+                <!-- <img src="/images/chart-graphic.svg" class="bottom-[-100%] 2xl:bottom-4 left-1/2 absolute h-[50px] -translate-x-1/2" alt="Wykres nastroju"> -->
             </div>
             <div class="bg-bg-tint p-6 rounded-xl text-center">
-                <p class="font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl">Porada na dziś</p>
+                <p class="font-semibold text-header-gray text-lg md:text-xl">Porada na dziś</p>
                 <p class="mt-2 text-gray-500 text-xs md:text-sm 2xl:text-base">Porada odświeżająca się raz dziennie lub przy każdym odwiedzeniu dashboard (do zdecydowania), można znaleść jakieś API. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptate? </p>
             </div>
         </div>
 
         <div class="gap-6 grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 min-h-100">
             <div class="lg:col-span-2 2xl:col-span-3 bg-bg-tint p-6 rounded-xl">
-                <p class="mb-5 font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl text-center">Wykres nastroju z ostatnich 30 dni</p>
+                <p class="mb-5 font-semibold text-header-gray text-lg md:text-xl text-center">Wykres nastroju z ostatnich 30 dni</p>
                 <div class="bg-gradient-to-b from-teal-300 to-white rounded">
                     <canvas id="moodChart"></canvas>
                 </div>
             </div>
             <div class="gap-6 grid grid-rows-2 h-full">
                 <a href="emotions_journal" class="block relative bg-bg-tint p-4 pr-8 md:pr-4 rounded-xl hover:ring-2 hover:ring-accent transition duration-300 cursor-pointer">
-                    <h3 class="mb-2 text-lg md:text-xl 2xl:text-2xl">Twój ostatni wpis</h3>
+                    <h3 class="mb-2 text-lg md:text-xl">Twój ostatni wpis</h3>
                     <p class="text-gray-500 text-xs md:text-sm">Lorem Ipsum dolore set amet ipsum randon, lorus deus machina...</p>
                     <p class="bottom-4 absolute text-gray-400 text-xs md:text-sm">09.04.2025</p>
                     <i class="fa-chevron-right right-4 bottom-4 absolute text-accent text-2xl md:text-3xl fa-solid"></i>
                 </a>
                 <a href="/mind_exercises" class="block relative bg-bg-tint p-4 pr-8 md:pr-4 rounded-xl hover:ring-2 hover:ring-accent transition duration-300 cursor-pointer">
-                    <h3 class="mb-2 text-lg md:text-xl 2xl:text-2xl">Polecane ćwiczenie</h3>
+                    <h3 class="mb-2 text-lg md:text-xl">Polecane ćwiczenie</h3>
                     <p class="text-xs md:text-sm text-accent-strong">2h Spacer - Łatwy</p>
-                    <p class="mt-2 text-gray-400 text-xs md:text-sm">Opis ćwiczenia Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    <p class="mt-2 text-gray-500 text-xs md:text-sm">Opis ćwiczenia Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                     <i class="fa-chevron-right right-4 bottom-4 absolute text-accent text-2xl md:text-3xl fa-solid"></i>
                 </a>
             </div>
--- mindsync/resources/views/account/emotionsReport.blade.php
+++ mindsync/resources/views/account/emotionsReport.blade.php
@@ -9,12 +9,123 @@
     <script src="https://kit.fontawesome.com/086b12d3c8.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="overflow-x-hidden flex min-h-screen">
+<body class="flex min-h-screen overflow-x-hidden">
     @include('partials.aside')
-    <main>
-        <h1>Analiza Emocji</h1>
-    </main>
+    <main class="flex-1 space-y-6 p-2 md:p-10 h-screen overflow-y-auto">
+    <div class="relative bg-bg-tint p-6 md:px-15 rounded-xl overflow-hidden">
+        <h2 class="font-semibold text-xl md:text-2xl 2xl:text-3xl">Analiza emocji</h2>
+        <p class="mt-2 text-gray-500 text-sm md:text-base">Zobacz swoje statystyki i interesujące informacje o twoim samopoczuciu</p>
+        <img src="/images/leaf1.svg" class="top-[-104px] left-[-100%] md:left-[-111px] z-3 absolute pointer-events-none" alt="leaf1">   
+        <img src="/images/leaf2.svg" class="top-[-75px] left-[596px] z-1 absolute pointer-events-none" alt="leaf2">   
+        <img src="/images/leaf3.svg" class="right-[-200%] 2xl:right-0! bottom-[-110px] z-0 absolute pointer-events-none" alt="leaf3">  
+    </div>
+
+    <div class="gap-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 grid-flow-dense">
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="text-lg md:text-xl">Przeważające Emocje</h3>
+            <p class="my-2 font-bold text-accent text-xl">Radość</p>
+            <p class="text-gray-500 text-sm md:text-base">Uczucie szczęścia i zadowolenia</p>
+        </div>
+        <div class="md:col-span-2 bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="text-lg md:text-xl">Co mówią dane</h3>
+            <p class="mt-2 text-gray-500 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="text-lg md:text-xl">Sugestia #1</h3>
+            <p class="mt-2 text-gray-500 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+
+    <div class="bg-bg-tint p-6 rounded-xl">
+        <div class="flex flex-wrap gap-4 mb-4 border-b border-accent">
+            <button class="px-4 py-2 rounded-t-xl cursor-pointer tabBtn--active week-report-btn">Raport tygodniowy</button>
+            <button class="px-4 py-2 rounded-t-xl cursor-pointer tabBtn--normal month-report-btn">Raport miesięczny</button>
+        </div>
+        <p class="text-gray-500 text-sm week-report">
+            Week Report <br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur reprehenderit nisi consectetur laboriosam? Odit, inventore dolorem eius perspiciatis alias repudiandae itaque magnam nesciunt, quasi culpa molestias, similique facilis iste? Repudiandae!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur reprehenderit nisi consectetur laboriosam? Odit, inventore dolorem eius perspiciatis alias repudiandae itaque magnam nesciunt, quasi culpa molestias,
+        </p>
+        <p class="hidden text-gray-500 text-sm month-report">
+            Month Report <br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur reprehenderit nisi consectetur laboriosam? Odit, inventore dolorem eius perspiciatis alias repudiandae itaque magnam nesciunt, quasi culpa molestias, similique facilis iste? Repudiandae!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur reprehenderit nisi consectetur laboriosam? Odit, inventore dolorem eius perspiciatis alias repudiandae itaque magnam nesciunt, quasi culpa molestias,
+        </p>
+    </div>
+
+    <div class="bg-bg-tint p-6 rounded-xl">
+        <h3 class="mb-5 font-semibold text-header-gray text-lg md:text-xl 2xl:text-2xl text-center">Wykres nastroju z ostatnich 30 dni</h3>
+        <div class="bg-gradient-to-b from-teal-300 to-white rounded">
+            <canvas id="mainMoodChart"></canvas>
+        </div>
+    </div>
+
+    <div class="gap-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="font-semibold text-header-gray text-lg md:text-xl">Twój średni nastrój</h3>
+            <div class="mt-5 font-bold text-accent text-4xl md:text-5xl">70%</div>
+        </div>
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="font-semibold text-header-gray text-lg md:text-xl">Streak aktywnych dni</h3>
+            <div class="mt-5 font-bold text-accent text-4xl md:text-5xl">14</div>
+        </div>
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="font-semibold text-header-gray text-lg md:text-xl">Twoje wpisy</h3>
+            <div class="mt-5 font-bold text-accent text-4xl md:text-5xl">253</div>
+        </div>
+        <div class="bg-bg-tint p-6 rounded-xl text-center">
+            <h3 class="font-semibold text-header-gray text-lg md:text-xl">Liczba wykonanych ćwiczeń</h3>
+            <div class="mt-5 font-bold text-accent text-4xl md:text-5xl">7</div>
+        </div>
+    </div>
+
+    <!-- Dodatkowe wykresy -->
+    <div class="gap-6 grid grid-cols-1 2lg:grid-cols-2">
+        <div class="bg-bg-tint p-6 rounded-xl">
+            <h3 class="mb-4 font-semibold text-lg md:text-xl text-center">Wykres 1</h3>
+            <div class="bg-gradient-to-b from-teal-300 to-white rounded">
+                <canvas id="chart1"></canvas>
+            </div>
+        </div>
+        <div class="bg-bg-tint p-6 rounded-xl">
+            <h3 class="mb-4 font-semibold text-lg md:text-xl text-center">Wykres 3</h3>
+            <div class="bg-gradient-to-b from-teal-300 to-white rounded">
+                <canvas id="chart3"></canvas>
+            </div>
+        </div>
+        <div class="2lg:col-span-2 bg-bg-tint p-6 rounded-xl">
+            <h3 class="mb-4 font-semibold text-lg md:text-xl text-center">Wykres 2</h3>
+            <div class="bg-gradient-to-b from-teal-300 to-white rounded">
+                <canvas id="chart2"></canvas>
+            </div>
+        </div>
+    </div>
+</main>
+
     
 </body>
+    <script>
+        const weekReport = document.querySelector('.week-report');
+        const monthReport = document.querySelector('.month-report');
+        const weekButton = document.querySelector('.week-report-btn');
+        const monthButton = document.querySelector('.month-report-btn');
+        weekButton.addEventListener('click', () => {
+            weekReport.classList.remove('hidden');
+            monthReport.classList.add('hidden');
 
+            weekButton.classList.add('tabBtn--active');
+            weekButton.classList.remove('tabBtn--normal');
+
+            monthButton.classList.remove('tabBtn--active');
+            monthButton.classList.add('tabBtn--normal');
+        });
+        monthButton.addEventListener('click', () => {
+            monthReport.classList.remove('hidden');
+            weekReport.classList.add('hidden');
+
+            monthButton.classList.add('tabBtn--active');
+            monthButton.classList.remove('tabBtn--normal');
+            
+            weekButton.classList.remove('tabBtn--active');
+            weekButton.classList.add('tabBtn--normal');
+        });
+    </script>
 </html>
